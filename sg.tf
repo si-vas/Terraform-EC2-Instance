7fcc35916753d90483649274229407dfa8e62413
siva <ssiva22292@gmail.com>
2022-09-10 11:45:01 -0400

    aws_security_group added

---
+++ sg.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "allow_all" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-allow_all"
+  }
+}
