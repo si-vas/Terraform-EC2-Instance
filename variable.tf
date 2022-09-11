ee9eab11032a83b5dc2771ff749583a0de060673
si-vas <99237926+si-vas@users.noreply.github.com>
2022-09-11 02:11:26 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,6 @@
-variable "amis" {}
-variable "public_cidr" {}
 variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "public_cidr" {}
+variable "amis" {}
 variable "instance_type" {}
 variable "key_name" {}
